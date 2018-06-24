efa10a412191b1562643aa94736318602fd306bb
dohwan <dosio0102@gmail.com>
2018-06-24 23:29:56 +0900

    sign up error 기능 추가

--- app/members/views.py
+++ app/members/views.py
@@ -16,7 +16,7 @@ def login_view(request):
             login(request, user)
             return redirect('posts:post-list')
         else:
-            context={
+            context = {
                 'wrong_input': "Error: Can't sign in"
             }
             return render(request, 'members/login.html', context)
@@ -39,14 +39,40 @@ def sign_up(request):
         username = request.POST['username']
         password = request.POST['password']
         confirm_pw = request.POST['confirm_pw']
-        if password != confirm_pw:
+        # if password != confirm_pw:
+        #     context = {
+        #         'wrong_pw_message': 'Error: not matched password',
+        #         'input_id': username,
+        #         'input_pw': password,
+        #     }
+        #     return render(request, 'members/signup.html', context)
+        # elif not username:
+        #     context = {
+        #         'no_name_message': 'Error: plz input ID'
+        #     }
+        #     return render(request, 'members/signup.html', context)
+
+        # if username in [i.username for i in User.objects.all()]:
+        #     context = {
+        #         'existed_name': 'Error: existed id',
+        #         'username_list': [i.username for i in User.objects.all()],
+        #     }
+        #     return render(request, 'members/signup.html', context)
+        if password != confirm_pw or\
+                not username or\
+                username in [u.username for u in User.objects.all()] or \
+                not password:
             context = {
-                'wrong_pw_message': 'Error: password does not match',
+                'wrong_pw_message': 'Error: not matched password',
                 'input_id': username,
                 'input_pw': password,
+                'input_cpw': confirm_pw,
+                'no_name_message': 'Error: plz input ID',
+                'existed_username': 'Error: existed ID',
+                'username_list': [i.username for i in User.objects.all()],
+                'no_password_message': 'Error: plz input PW'
             }
             return render(request, 'members/signup.html', context)
-            # return HttpResponse(context['wrong_pw'])
         else:
             User.objects.create_user(
                 username=username,
