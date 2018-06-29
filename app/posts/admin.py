ea686f50cb91bc59c77a0815321ec86cb4e478f9
dohwan <dosio0102@gmail.com>
2018-06-29 10:38:24 +0900

    Add PostComment to Admin

--- app/posts/admin.py
+++ app/posts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from posts.models import Post
+from posts.models import Post, PostComment
 
 admin.site.register(Post)
+admin.site.register(PostComment)
